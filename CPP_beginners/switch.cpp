c63cd661f75564324f4ef87647563729f6f186d0
Anna Dunster <llama747@gmail.com>
2020-10-08 12:28:50 -0700

    Fun with switch statement and input verification!

---
+++ CPP_beginners/switch.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    int user_input = 0;
+
+    cout << "Bogus menu!!!" << endl;
+    while (user_input == 0) {
+    
+    cout << "\nPlease choose from one of the following meaningless options:\n" <<
+    "1: Absolutely nothing useful\n" << 
+    "2: I'm getting some practice here, okay?\n" <<
+    "3: Self talk is important to success\n" <<
+    "4: So you should try to talk nice to yourself\n" <<
+    "5: I mean it.\n" <<
+    "6: exit :(\n"
+    "> " << flush;
+
+    cin >> user_input;
+
+    if(cin.fail()) {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        user_input = 0;
+    }
+
+    switch (user_input) 
+    {
+    case 1: 
+        cout << "Are you sure??" << endl;
+        user_input = 0;
+        break;
+    case 2: 
+        cout << "YES PRACTICE, Good, do it! :chaos:" << endl;
+        user_input = 0;
+        break;
+    case 3: 
+        cout << "Super important, yes" << endl;
+        user_input = 0;
+        break;
+    case 4:
+        cout << "But, but..... it's hard!" << endl;
+        user_input = 0;
+        break;
+    case 5:
+        cout << ":gasp: okay I'll be good D: " << endl;
+        user_input = 0;
+        break;
+    case 6:
+        cout << "Goodbye :sob:" << endl;
+        break;
+    default:
+        cout << "You have to pick a number on the list or it doesn't work :(\n" << endl;
+        user_input = 0;
+    }
+    }
+    return 0;
+}
